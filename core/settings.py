e483193d84383e1cb4173f6727379525e89f8345
Mahir Hasan <75481410+mahir-bot@users.noreply.github.com>
2024-05-21 02:16:42 +0600

    Update settings.py Mail Cred

--- core/settings.py
+++ core/settings.py
@@ -140,7 +140,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 
-EMAIL_HOST_USER = 'mahirhasan333@gmail.com'
-EMAIL_HOST_PASSWORD = 'rsdwmcclrfmhwbup'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
-DEFAULT_FROM_EMAIL = 'mahirhasan333@gmail.com'
+DEFAULT_FROM_EMAIL = '@gmail.com'
